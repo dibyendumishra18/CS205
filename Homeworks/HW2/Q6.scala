61425f7dfc2d3e2c93f62c0372719463483e646a
Dibyendu Mishra <dibyendu.mishra_ug18@ashoka.edu.in>
2016-09-13 18:32:38 +0530

    Updating Q6.

--- Homeworks/HW2/Q6.scala
+++ Homeworks/HW2/Q6.scala
@@ -1,14 +1,22 @@
-object Q6{
-def quickSort(l:List[Int]):List[Int]={
-if (l.isEmpty) 
-    l
-else {
-    val (smaller, bigger) = l.tail partition (_ < l.head)
-    quickSort(smaller) ::: l.head :: quickSort(bigger)
-     }
-}
-def main(args:Array[String]):Unit={
-val l1=List(4,2,7,1,9)
-println(quickSort(l1))}
-}
 
+object Q6{
+def quickSort(l:List[Int]):List[Int] ={
+    l match{
+    case Nil => l
+case hd::rest => pivotSort(quickSort(l.filterNot(elm=>elm==l.max)),l.max)}}
+
+def pivotSort(x:List[Int], n: Int): List[Int]={
+
+    if(x == Nil)
+      x:+n
+    else if(x.head<=n)
+      x.head::pivotSort(x.tail, n)
+    else{      
+      pivotSort(x.tail, n):+x.head}}
+
+def main(args:Array[String]):Unit={
+val l1=List(1,4,9,3,7,5,10,11,13)
+println(quickSort(l1))}
+
+  
+}
